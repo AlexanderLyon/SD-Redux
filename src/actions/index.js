3fc46b1d83daa4b0b0b291bee2cbe74ceb09bc4e
alyon011@gmail.com <alyon011@gmail.com>
2020-01-23 16:24:31 -0500

    Create Redux store and add action creator using redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import sampleData from './sampleData';
+
+/**
+ * Makes the mock "API request"
+ */
+const fetchProducts = () => {
+  return new Promise((resolve) => {
+    window.setTimeout(() => {
+      resolve(sampleData.products);
+    }, 1000);
+  });
+}
+
+/**
+ * Retrieves product list from mock API
+ */
+export const fetchProductsFromAPI = () => {
+  return (dispatch) => {
+    return fetchProducts().then(
+      (resp) => dispatch({
+        type: 'UPDATE_PRODUCTS',
+        products: resp
+      })
+    );
+  };
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import productReducer from './reducers/products';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+const store = createStore(productReducer, applyMiddleware(thunk));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
