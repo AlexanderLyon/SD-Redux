d193d2b2593688cd063ce8826764510c60d76318
alyon011@gmail.com <alyon011@gmail.com>
2020-01-23 16:31:09 -0500

    Use Provider to make store available to nested components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux'
+import { fetchProductsFromAPI } from './actions';
 import './App.css';
 
-function App() {
+const App = (props) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Store</h1>
       </header>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: state
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchProducts: dispatch(fetchProductsFromAPI())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 import productReducer from './reducers/products';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 const store = createStore(productReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
